606ed393d7e174e19b6962ed80c72db63cd30575
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-04 05:21:58 +0700

    chore: setup pinia

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer
+    class="bg-[url('https://www.rockstargames.com/reddeadredemption2/dist/img/global/footer/673d686c4197dc1c54f4182acf5b33f9.png')] h-32 bg-cover bg-top"
+  ></footer>
+</template>
