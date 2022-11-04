50383c5f05db163eae94a62cb7aeffdb197bd76c
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-04 09:40:21 +0700

    chore: minor

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer
-    class="bg-[url('https://www.rockstargames.com/reddeadredemption2/dist/img/global/footer/673d686c4197dc1c54f4182acf5b33f9.png')] h-32 bg-cover bg-top"
+    class="bg-[url('https://www.rockstargames.com/reddeadredemption2/dist/img/global/footer/673d686c4197dc1c54f4182acf5b33f9.png')] h-32 z-20 bg-cover bg-top"
   ></footer>
 </template>
