9592b73b9b3e9bca6dcd39e86d2735eae9e65bcd
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-04 22:50:09 +0700

    chore: adding non exist page

--- src/data/featuresData.js
+++ src/data/featuresData.js
@@ -4,21 +4,21 @@ const featuresData = [
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/18.jpg",
     desc: "pc release",
-    to: "/",
+    to: "/pcrelease",
   },
   {
     id: 2,
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/15.jpg",
     desc: "reviews",
-    to: "/",
+    to: "/reviews",
   },
   {
     id: 3,
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/16.jpg",
     desc: "red dead online",
-    to: "/",
+    to: "/reddeadonline",
   },
   {
     id: 4,
@@ -39,28 +39,28 @@ const featuresData = [
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/12.jpg",
     desc: "gampelay video part 2",
-    to: "/",
+    to: "/gameplay",
   },
   {
     id: 7,
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/5.jpg",
     desc: "the van der linde gang",
-    to: "/",
+    to: "/vanderlindegang",
   },
   {
     id: 8,
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/11.jpg",
     desc: "wildlife",
-    to: "/",
+    to: "/wildlife",
   },
   {
     id: 9,
     image:
       "https://www.rockstargames.com/reddeadredemption2/rockstar_games/r_d_r2_core/img/home_features/17.jpg",
     desc: "soundtrack",
-    to: "/",
+    to: "/soundtrack",
   },
 ];
 
